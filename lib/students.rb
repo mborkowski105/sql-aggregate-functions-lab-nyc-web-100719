5da8c0dad8c325a837f200e19f0c810c1ebeda23
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-17 16:56:31 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,12 +5,15 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  "SELECT MIN(gpa) from students"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) from students"
 end
 
 def total_tardies_for_all_students
+  "SELECT COUNT(tardies) from students"
 end
 
 def average_gpa_for_9th_grade
